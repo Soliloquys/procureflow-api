a8701c67ae36682e9f1578a1de98a2df207b0daf
Simon Kwabla Azigie <simonziggy18@gmail.com>
2025-12-14 18:31:20 +0000

    set up role based authfor user model

--- procureflow/procureflow/settings.py
+++ procureflow/procureflow/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'vendors',
     'users',
     'quotes',
-    'requests',
+    'procurements',
     'projects',
     'payments',
     'analytics',
@@ -129,3 +129,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
--- procureflow/users/models.py
+++ procureflow/users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = 'ADMIN', 'Admin'
+        ENGINEER = 'ENGINEEER', 'Engineer'
+        PROCUREMENT_OFFICER = 'PROCUREMENT_OFFICER', 'Procurement Officer'
+        ACCOUNTANT = 'ACCOUNTANT', 'Accountant'
+        PROJECT_MANAGER = 'PROJECT_MANAGER', 'Project Manager'
+
+        role = models.CharField(
+            max_length=30,
+            choices=Role.choices,
+            default=Role.ENGINEER, 
+        )
+
+        phone = models.CharField(
+            max_length=15, 
+            blank=True, 
+            null=True
+        )
+        def __str__(self):
+            return f"{self.username} ({self.role})"
